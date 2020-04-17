c2f105577b42a61e2c847375434c430157ef5334
aniketjha1729 <kumaraniket1729@gmail.com>
2020-04-18 04:16:22 +0530

    front end base

---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Base from '../core/Base'
+
+export default function Signin() {
+    return (
+        <Base title="Sign In" description="A Page for SignIn!">
+            <h1>Signin</h1>
+        </Base>
+    )
+}
---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+import Base from '../core/Base'
+
+export default function Signup() {
+    return (
+        <Base title="Sign Up" description="A Page for Signup!">
+            <h1>Signup</h1>
+        </Base>
+    )
+}
