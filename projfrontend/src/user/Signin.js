f2ba12938fa857016cf24014e6d589ed235aba31
aniketjha1729 <kumaraniket1729@gmail.com>
2020-04-20 18:54:29 +0530

    SignIn and SingUp base

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import Base from '../core/Base'
 
-export default function Signin() {
+const Signin=() =>{
+    
+    const signInForm = () => {
+      return (
+        <div className="row">
+          <div className="col-md-6 offset-sm-3 text-left">
+            <form>
+              <div className="form-group">
+                <label className="text-light">Email</label>
+                <input
+                //   onChange={handleChange("email")}
+                //   value={email}
+                  className="form-control"
+                  type="email"
+                />
+              </div>
+
+              <div className="form-group">
+                <label className="text-light">Password</label>
+                <input
+                //   onChange={handleChange("password")}
+                //   value={password}
+                  className="form-control"
+                  type="password"
+                />
+              </div>
+              <button  className="btn btn-success btn-block">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      );
+    };
+    
+    
     return (
         <Base title="Sign In" description="A Page for SignIn!">
-            <h1>Signin</h1>
+            {signInForm()}
         </Base>
     )
 }
+export default Signin;
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,10 +1,57 @@
 import React,{useState} from 'react'
 import Base from '../core/Base'
 
-export default function Signup() {
-    return (
-        <Base title="Sign Up" description="A Page for Signup!">
-            <h1>Signup</h1>
+const Signup = ()=>{
+    
+    const signUpForm = () => {
+      return (
+        <div className="row">
+          <div className="col-md-6 offset-sm-3 text-left">
+            <form>
+              <div className="form-group">
+                <label className="text-light">Name</label>
+                <input
+                  className="form-control"
+                //   onChange={handleChange("name")}
+                  type="text"
+                //   value={name}
+                />
+              </div>
+              <div className="form-group">
+                <label className="text-light">Email</label>
+                <input
+                  className="form-control"
+                //   onChange={handleChange("email")}
+                  type="email"
+                //   value={email}
+                />
+              </div>
+
+              <div className="form-group">
+                <label className="text-light">Password</label>
+                <input
+                //   onChange={handleChange("password")}
+                  className="form-control"
+                  type="password"
+                //   value={password}
+                />
+              </div>
+              <button  className="btn btn-success btn-block">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      );
+    };
+    
+    
+    
+    return(
+        <Base title="Sign Up Page" description="A page for SignUp">
+        {signUpForm()}
         </Base>
+        
     )
 }
+export default Signup;
