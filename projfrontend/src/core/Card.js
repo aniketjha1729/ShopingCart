9580f478dd5f42258a8f2d0fdedc8db4ebbc3a13
aniketjha1729 <kumaraniket1729@gmail.com>
2020-04-30 14:00:19 +0530

    add cart remove cart working

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import ImageHelper from './helper/ImageHelper';
+import { Redirect } from "react-router-dom";
+import { addItemToCart, removeItemFromCart } from './helper/cartHelper';
 
 
-const Card = ({ product,addtoCart=true, removetoCart=false}) => {
+const Card = ({ product,addtoCart=true, removefromCart=false,setReload=f=>f,reload=undefined}) => {
 
-
+        const [redirect, setRedirect] = useState(false);
+        const [count, setCount] = useState(product.count)
         const cartTitle=product?product.name:"A photo from pexels"   
         const cartDescription=product?product.description:"Default"    
         const cartPrice=product?product.price:"Default"
-    
+
+
+        const addToCart=()=>{
+            addItemToCart(product,()=> setRedirect(true))
+        }
+
+
+        const getARedirect=(redirect)=>{
+            if(redirect){
+                return <Redirect to ="/cart" />
+            }
+        }
+
         const showAddToCart=(addtoCart)=>{
             return(
                 addtoCart &&(
                     <button
-                        onClick={() => { }}
+                        onClick={addToCart}
                         className="btn btn-block btn-outline-success mt-2 mb-2">
                         Add to Cart
                     </button>
@@ -21,11 +36,14 @@ const Card = ({ product,addtoCart=true, removetoCart=false}) => {
             )
         }
 
-        const showRemoveToCart=(removetoCart)=>{
+        const showRemoveToCart=(removefromCart)=>{
             return(
-                removetoCart &&(
+                removefromCart &&(
                     <button
-                        onClick={() => { }}
+                        onClick={() => {
+                            removeItemFromCart(product._id)
+                            setReload(!reload)
+                         }}
                         className="btn btn-block btn-outline-danger mt-2 mb-2">
                         Remove from cart
                     </button>
@@ -36,6 +54,7 @@ const Card = ({ product,addtoCart=true, removetoCart=false}) => {
             <div className="card text-white bg-dark border border-info ">
                 <div className="card-header lead">{cartTitle}</div>
                 <div className="card-body">
+                    {getARedirect(redirect)}
                     <ImageHelper product={product}/>
                     <p className="lead bg-success font-weight-normal text-wrap">
                         {cartDescription}
@@ -46,7 +65,7 @@ const Card = ({ product,addtoCart=true, removetoCart=false}) => {
                             {showAddToCart(addtoCart)}
                         </div>
                         <div className="col-12">
-                            {showRemoveToCart(removetoCart)}
+                            {showRemoveToCart(removefromCart)}
                         </div>
                     </div>
                 </div>
